4915393e5f2f06d817740e3af8d80f34e9b82ef2
WittySmirk <wyattdegenhart@gmail.com>
2025-04-18 22:02:56 -0600

    fixed gothic to have a store

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,11 +1,19 @@
 package auth
 
 import (
+	"github.com/gorilla/sessions"
 	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/google"
 	"os"
 )
 
+var sessionSecret = []byte(os.Getenv("SESSION_SECRET"))
+
 func CreateAuth() {
+	store := sessions.NewCookieStore(sessionSecret)
+	store.Options.HttpOnly = true
+	store.Options.Secure = false
+	gothic.Store = store
 	goth.UseProviders(google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_SECRET"), os.Getenv("BACKEND_URL")+"/api/auth/google/callback"))
 }
